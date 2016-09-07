89d31c6c67cfe991d8392227705a4ed34337b0c3
Abraham Isaac Durán <abrahamduran@abrahams-macbook-pro.local>
2016-09-07 16:55:11 -0400

    +Added id property for Friend class +Added SearchBar outlet +Added Section structure +Added search feature +Added FollowFriend feature

--- Friends/FollowFriendsViewController.swift
+++ Friends/FollowFriendsViewController.swift
@@ -8,39 +8,71 @@
 
 import UIKit
 
-class FollowFriendsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+extension UIView {
+    func rotate(toValue: CGFloat, duration: CFTimeInterval = 0.2, completionDelegate: AnyObject? = nil) {
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.toValue = toValue
+        rotateAnimation.duration = duration
+        rotateAnimation.removedOnCompletion = false
+        rotateAnimation.fillMode = kCAFillModeForwards
+        
+        if let delegate: AnyObject = completionDelegate {
+            rotateAnimation.delegate = delegate
+        }
+        self.layer.addAnimation(rotateAnimation, forKey: nil)
+    }
+}
+
+class FollowFriendsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    struct Section {
+        var name: String
+        var collapsed: Bool!
+        var items: [Friend]!
+        
+        init(name: String, items: [Friend]){
+            self.name = name
+            self.collapsed = !(items.count > 0)
+            self.items = items
+        }
+    }
 
     @IBOutlet weak var friendsListTableView: UITableView!
-    var friendsList: [FriendsList]!
+    @IBOutlet weak var searchField: UISearchBar!
+    var dataSource: [Section]!
+    var sectionsList: [Section]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         friendsListTableView.dataSource = self
         friendsListTableView.delegate = self
+        searchField.delegate = self
         
         let addressBookContacts: [Friend] = [
-            Friend(name: "Markus Feehily"),
-            Friend(name: "Kelly Egan", isFriend: true)
+            Friend(id: 1, name: "Markus Feehily"),
+            Friend(id: 2, name: "Kelly Egan", isFriend: true)
         ]
         
         let facebookContacts: [Friend] = [
-            Friend(name: "Shane Filan"),
-            Friend(name: "Markus"),
-            Friend(name: "Derrick Lacey")
+            Friend(id: 3, name: "Shane Filan"),
+            Friend(id: 4, name: "Markus"),
+            Friend(id: 5, name: "Derrick Lacey")
         ]
         
         let twitterContacts: [Friend] = [
-            Friend(name: "Led Zeppelin"),
-            Friend(name: "Johh Lenon"),
-            Friend(name: "Pink Floyd")
+            Friend(id: 6, name: "Led Zeppelin"),
+            Friend(id: 7, name: "Johnhn Lenon"),
+            Friend(id: 8, name: "Pink Floyd")
         ]
         
-       friendsList = [
-            FriendsList(source: "Address Book", friends: addressBookContacts),
-            FriendsList(source: "Facebook", friends: facebookContacts),
-            FriendsList(source: "Twitter", friends: twitterContacts)
+        dataSource = [
+            Section(name: "Address Book", items: addressBookContacts),
+            Section(name: "Facebook", items: facebookContacts),
+            Section(name: "Twitter", items: twitterContacts)
         ]
+        
+        sectionsList = dataSource
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,7 +81,7 @@ class FollowFriendsViewController: UIViewController, UITableViewDataSource, UITa
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return friendsList.count
+        return sectionsList.count
     }
     
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -57,36 +89,76 @@ class FollowFriendsViewController: UIViewController, UITableViewDataSource, UITa
     }
     
     func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let header: CollapsibleTableViewHeader = tableView.dequeueReusableCellWithIdentifier("headerCell") as! CollapsibleTableViewHeader
-        let header: FriendHeaderTableViewCell = tableView.dequeueReusableHeaderFooterViewWithIdentifier("headerCell") as! FriendHeaderTableViewCell
-//
-//        header.toggleButton.tag = section
-//        header.imageHeader.image = UIImage(named: "default.png")
-        header.nameLabel.text = "Some Text"
+        let header = tableView.dequeueReusableCellWithIdentifier("header") as! CollapsibleTableViewHeader
+        
+        header.titleLabel.text = sectionsList[section].name
+        header.toggleButton.rotate(sectionsList[section].collapsed! ? 0.0 : CGFloat(M_PI_2))
+        header.toggleButton.tag = section
+        header.toggleButton.addTarget(self, action: #selector(self.toggleCollapse), forControlEvents: .TouchUpInside)
         
         return header.contentView
     }
 
-    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int
-    {
-        return friendsList[section].friends.count
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return (sectionsList[section].collapsed!) ? 0 : sectionsList[section].items.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    {
-        let friend = friendsList[indexPath.section].friends[indexPath.row]
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let friend = sectionsList[indexPath.section].items[indexPath.row]
         
         let friendCell: FriendTableViewCell = tableView.dequeueReusableCellWithIdentifier("friendCell", forIndexPath: indexPath) as! FriendTableViewCell
         friendCell.nameLabel.text = friend.name
         friendCell.friendSwitch.on = friend.isFriend!
+        friendCell.friendSwitch.tag = friend.id
+        friendCell.friendSwitch.addTarget(self, action: #selector(self.followFriend), forControlEvents: .TouchUpInside)
         let image: UIImage = UIImage(named: friend.profilePicturePath!)!
-//        friendCell.profilePicture = UIImageView(frame: CGRectMake(0, 0, 100, 100))
-        
-//        friendCell.profilePicture.layer.masksToBounds = false
-//        friendCell.profilePicture.layer.cornerRadius = friendCell.profilePicture.frame.size.width / 2
-//        friendCell.profilePicture.clipsToBounds = true
         friendCell.profilePicture.image = image
         return friendCell
+    }
+    
+    func toggleCollapse(sender: UIButton) {
+        let section = sender.tag
+        
+        sectionsList[section].collapsed = !(sectionsList[section].collapsed)
+        
+        friendsListTableView.reloadSections(NSIndexSet(index: section), withRowAnimation: .Automatic)
+    }
+    
+    func followFriend(sender: UIButton) {
+        let friendId = sender.tag
+        for source in dataSource {
+            for friend in source.items {
+                if (friend.id == friendId) {
+                    friend.isFriend = true
+                    break
+                }
+            }
+        }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.searchField.endEditing(true)
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        var results = [Section]()
+        if (!searchText.isEmpty) {
+            for source in dataSource {
+                var friendResults = [Friend]()
+                for friend in source.items {
+                    if (friend.name.lowercaseString.containsString(searchText.lowercaseString)) {
+                        friendResults.append(friend)
+                    }
+                }
+                results.append(Section(name: source.name, items: friendResults))
+            }
+            
+            sectionsList = results
+        } else {
+            sectionsList = dataSource
+        }
+        
+        friendsListTableView.reloadData()
     }
 }
 
--- Friends/Friend.swift
+++ Friends/Friend.swift
@@ -9,29 +9,31 @@
 import Foundation
 
 class Friend {
+    var id: Int
     var name: String
     var profilePicturePath: String?
     var isFriend: Bool?
     
-    init(name: String, profilePicturePath: String, isFriend: Bool){
+    init(id: Int, name: String, profilePicturePath: String, isFriend: Bool){
+        self.id = id
         self.name = name
         self.profilePicturePath = profilePicturePath
         self.isFriend = isFriend
     }
     
     convenience init(){
-        self.init(name: "", profilePicturePath: "default.png", isFriend: false)
+        self.init(id: 0, name: "", profilePicturePath: "default.png", isFriend: false)
     }
     
-    convenience init(name: String){
-        self.init(name: name, profilePicturePath: "default.png", isFriend: false)
+    convenience init(id: Int, name: String){
+        self.init(id: id, name: name, profilePicturePath: "default.png", isFriend: false)
     }
     
-    convenience init(name: String, profilePicturePath: String){
-        self.init(name: name, profilePicturePath: profilePicturePath, isFriend: false)
+    convenience init(id: Int, name: String, profilePicturePath: String){
+        self.init(id: id, name: name, profilePicturePath: profilePicturePath, isFriend: false)
     }
     
-    convenience init(name: String, isFriend: Bool){
-        self.init(name: name, profilePicturePath: "default.png", isFriend: isFriend)
+    convenience init(id: Int, name: String, isFriend: Bool){
+        self.init(id: id, name: name, profilePicturePath: "default.png", isFriend: isFriend)
     }
 }
